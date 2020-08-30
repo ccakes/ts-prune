53479e88e9f0b9867949b0eda6ec0072ad348eb5
Cameron Daniel <cam.daniel@gmail.com>
2020-08-30 09:47:22 +0200

    fix issue where file.getDescendantAtPos is null

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -19,7 +19,7 @@ type OnResultType = (result: IAnalysedResult) => void;
 
 export enum AnalysisResultTypeEnum {
   POTENTIALLY_UNUSED,
-  DEFINITELY_USED
+  DEFINITELY_USED,
 }
 
 export type ResultSymbol = {
@@ -32,10 +32,10 @@ export type IAnalysedResult = {
   file: string;
   type: AnalysisResultTypeEnum;
   symbols: ResultSymbol[];
-}
+};
 
 function handleExportDeclaration(node: SourceFileReferencingNodes) {
-  return (node as ExportDeclaration).getNamedExports().map(n => n.getName());
+  return (node as ExportDeclaration).getNamedExports().map((n) => n.getName());
 }
 
 function handleImportDeclaration(node: SourceFileReferencingNodes) {
@@ -43,7 +43,7 @@ function handleImportDeclaration(node: SourceFileReferencingNodes) {
 
   (node as ImportDeclaration)
     .getNamedImports()
-    .map(n => referenced.push(n.getName()));
+    .map((n) => referenced.push(n.getName()));
 
   const defaultImport = (node as ImportDeclaration).getDefaultImport();
 
@@ -72,9 +72,10 @@ const mustIgnore = (symbol: Symbol, file: SourceFile) => {
     .map((decl) => decl.getStartLinePos())
     .reduce((currentMin, current) => Math.min(currentMin, current), Infinity);
 
-  const comments = file
-    .getDescendantAtPos(symbolLinePos)
-    .getLeadingCommentRanges();
+  const descendant = file.getDescendantAtPos(symbolLinePos);
+  if (!descendant) return false;
+
+  const comments = descendant.getLeadingCommentRanges();
 
   if (!comments) {
     return false;
@@ -84,7 +85,10 @@ const mustIgnore = (symbol: Symbol, file: SourceFile) => {
 };
 
 const lineNumber = (symbol: Symbol) =>
-  symbol.getDeclarations().map(decl => decl.getStartLineNumber()).reduce((currentMin, current) => Math.min(currentMin, current), Infinity)
+  symbol
+    .getDeclarations()
+    .map((decl) => decl.getStartLineNumber())
+    .reduce((currentMin, current) => Math.min(currentMin, current), Infinity);
 
 export function getExported(file: SourceFile) {
   return file
@@ -92,48 +96,49 @@ export function getExported(file: SourceFile) {
     .filter((symbol) => !mustIgnore(symbol, file))
     .map((symbol) => ({
       name: symbol.compilerSymbol.name,
-      line: lineNumber(symbol)
+      line: lineNumber(symbol),
     }));
 }
 
 const importWildCards = (file: SourceFile) =>
   file
     .getImportDeclarations()
-    .map(decl => ({
+    .map((decl) => ({
       moduleSourceFile: getModuleSourceFile(decl),
-      definitelyUsed: isDefinitelyUsedImport(decl)
+      definitelyUsed: isDefinitelyUsedImport(decl),
     }))
-    .filter(meta => meta.definitelyUsed && !!meta.moduleSourceFile)
+    .filter((meta) => meta.definitelyUsed && !!meta.moduleSourceFile)
     .map(({ moduleSourceFile }) => ({
       file: moduleSourceFile,
       symbols: [],
-      type: AnalysisResultTypeEnum.DEFINITELY_USED
+      type: AnalysisResultTypeEnum.DEFINITELY_USED,
     }));
-
 
 const exportWildCards = (file: SourceFile) =>
   file
     .getExportDeclarations()
-    .filter(decl => decl.getText().includes("*"))
+    .filter((decl) => decl.getText().includes("*"))
     .map((decl) => ({
       file: getModuleSourceFile(decl),
       symbols: [],
-      type: AnalysisResultTypeEnum.DEFINITELY_USED
+      type: AnalysisResultTypeEnum.DEFINITELY_USED,
     }));
 
 const emitDefinitelyUsed = (file: SourceFile, onResult: OnResultType) => {
-  [
-    ...importWildCards(file),
-    ...exportWildCards(file),
-  ].forEach(onResult);
+  [...importWildCards(file), ...exportWildCards(file)].forEach(onResult);
 };
 
 const emitPotentiallyUnused = (file: SourceFile, onResult: OnResultType) => {
   const exported = getExported(file);
 
   const idsInFile = file.getDescendantsOfKind(ts.SyntaxKind.Identifier) || [];
-  const referenceCounts = countBy(identity, (idsInFile || []).map(node => node.getText()));
-  const referencedInFile = Object.entries(referenceCounts).flatMap(([name, count]) => count > 1 ? [name] : []);
+  const referenceCounts = countBy(
+    identity,
+    (idsInFile || []).map((node) => node.getText())
+  );
+  const referencedInFile = Object.entries(
+    referenceCounts
+  ).flatMap(([name, count]) => (count > 1 ? [name] : []));
 
   const referenced2D = file
     .getReferencingNodesInOtherSourceFiles()
@@ -149,26 +154,40 @@ const emitPotentiallyUnused = (file: SourceFile, onResult: OnResultType) => {
 
   const referenced = ([] as string[]).concat(...referenced2D);
 
-  const unused = referenced.includes("*") ? [] :
-    exported.filter(exp => !referenced.includes(exp.name))
-      .map(exp => ({ ...exp, usedInModule: referencedInFile.includes(exp.name) }))
+  const unused = referenced.includes("*")
+    ? []
+    : exported
+        .filter((exp) => !referenced.includes(exp.name))
+        .map((exp) => ({
+          ...exp,
+          usedInModule: referencedInFile.includes(exp.name),
+        }));
 
   onResult({
     file: realpathSync(file.getFilePath()),
     symbols: unused,
-    type: AnalysisResultTypeEnum.POTENTIALLY_UNUSED
+    type: AnalysisResultTypeEnum.POTENTIALLY_UNUSED,
   });
 };
 
-const emitTsConfigEntrypoints = (entrypoints: string[], onResult: OnResultType) =>
-  entrypoints.map(file => ({
-    file,
-    symbols: [],
-    type: AnalysisResultTypeEnum.DEFINITELY_USED,
-  })).forEach(emittable => onResult(emittable))
+const emitTsConfigEntrypoints = (
+  entrypoints: string[],
+  onResult: OnResultType
+) =>
+  entrypoints
+    .map((file) => ({
+      file,
+      symbols: [],
+      type: AnalysisResultTypeEnum.DEFINITELY_USED,
+    }))
+    .forEach((emittable) => onResult(emittable));
 
-export const analyze = (project: Project, onResult: OnResultType, entrypoints: string[]) => {
-  project.getSourceFiles().forEach(file => {
+export const analyze = (
+  project: Project,
+  onResult: OnResultType,
+  entrypoints: string[]
+) => {
+  project.getSourceFiles().forEach((file) => {
     emitPotentiallyUnused(file, onResult);
     emitDefinitelyUsed(file, onResult);
   });
